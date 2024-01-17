512e2fb6536a84d1292f9348c4ad4bcd94665e09
Isaiah Jones Jr <111946561+isaiahjonesjr@users.noreply.github.com>
2024-01-17 16:33:34 -0600

    uploaded src files

---
+++ app.js
@@ -0,0 +1,12 @@
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+}
+
+function hideSidebar()
+{
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+
+    // hide the side bar after clicking on the link too
+}
